df76b4658a1d6e05b68a062c2175f956cb1be915
“T3sultan” <incognito.soul011@gmail.com>
2021-10-29 19:42:39 +0600

    footer

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBA_xgojx53L0ecja2tLpZK76snbYItu_k",
-    authDomain: "assignment-11-9e433.firebaseapp.com",
-    projectId: "assignment-11-9e433",
-    storageBucket: "assignment-11-9e433.appspot.com",
-    messagingSenderId: "328927581596",
-    appId: "1:328927581596:web:2639230b473bfa974fe459"
-  };
-export default firebaseConfig;  
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+export default firebaseConfig;
