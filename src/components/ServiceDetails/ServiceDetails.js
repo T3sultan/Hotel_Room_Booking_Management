38e60e79cefb34576ddd3164731f84bba040630a
“T3sultan” <incognito.soul011@gmail.com>
2021-10-31 04:20:17 +0600

    order place

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import './ServiceDetails.css'
+import { useForm } from "react-hook-form";
 
 const ServiceDetails = () => {
     const { serviceId } = useParams();
     const [service, setService] = useState({})
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
 
     useEffect(() => {
-      
+
         fetch(`http://localhost:5000/services/${serviceId}`)
             .then(res => res.json())
             .then(data => setService(data))
@@ -16,9 +20,67 @@ const ServiceDetails = () => {
     }, []);
     return (
         <div>
-            <h2>Details of: {service.name}</h2>
-            <h2>Booking :{serviceId}</h2>
+            <h2 className="orderPlace">Order Place</h2>
+            <div className="container4 my-4">
+
+                <div className="details1">
+
+                    <Card className="detailsStyle" style={{ width: '30rem', alignItems: 'center' }}>
+                        <Card.Img variant="top" className="w-100" src={service?.image} />
+                        <Card.Title>{service?.description}</Card.Title>
+                        <Card.Body>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <input
+                                    {...register("title")}
+                                    placeholder="Title"
+                                    className="p-2 m-2 w-100"
+                                    value={service?.title}
+                                   
+                                />
+                                <br />
+                                <input
+                                    {...register("name")}
+                                    placeholder="Name"
+                                    className="p-2 m-2 w-100"
+                                    value={service?.name}
+                                />
+                                <br />
+                                <input
+                                    {...register("email")}
+                                    placeholder="Email"
+                                    className="p-2 m-2 w-100"
+                                    value={service?.email}
+                                />
+                                <br />
+                                <input
+                                    {...register("price")}
+                                    placeholder="price"
+                                    className="p-2 m-2 w-100"
+                                    type="number"
+                                    value={service?.price}
+                                />
+                                <br />
+                                <input
+                                    {...register("date")}
+                                    placeholder="Name"
+                                    type="date"
+                                    className="p-2 m-2 w-100"
+                                    value={service?.date}
+                                />
+                                <br />
+                               
+                                {errors.exampleRequired && <span>This field is required</span>}
+
+                                <input type="submit" value="Submit" className="btn btn-info w-100 btnStyle1" />
+                            </form>
+                        </Card.Body>
+                    </Card>
+                </div>
+
+            </div>
+
         </div>
+
     );
 };
 
